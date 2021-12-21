ed50d3ad88d390f800a27e9ef49577a4aa34dcca
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-12-21 19:52:21 +0100

    Remove libtidy, fix encoding of entities, fix <script> being encoded with html entities.
    By removing libtidy we get to see what the parser actually does itself. There's still etree in front of it to make sure it gets valid sgml at least.

--- tagtransfer/html_translator.py
+++ tagtransfer/html_translator.py
@@ -1,20 +1,7 @@
 import bergamot
 from bergamot import Service, Response, ResponseOptions, ServiceConfig, TranslationModel
 import cssselect
-import tidylib
 from lxml import html, etree
-
-BASE_OPTIONS = {
-  "indent": 1,           # Pretty; not too much of a performance hit
-  "tidy-mark": 0,        # No tidy meta tag in output
-  "wrap": 0,             # No wrapping
-  "alt-text": "",        # Help ensure validation
-  "doctype": 'html5',   # Little sense in transitional for tool-generated markup...
-  "force-output": 1,     # May not get what you expect but you will get something
-  "output-html": 1,
-  "drop-empty-elements": 0,
-  "drop-empty-paras": 0,
-}
 
 class FakeAnnotatedText:
     def __init__(self, text):
@@ -25,9 +12,7 @@ class FakeResponse:
         self.target = FakeAnnotatedText(text)
 
 def convert(node):
-    render = etree.tostring(node).decode("utf-8")
-    tidy, errors = tidylib.tidy_fragment(render, BASE_OPTIONS)
-    return tidy
+    return etree.tostring(node, method='html', encoding='utf-8').decode("utf-8")
 
 class HTMLTranslator:
     def __init__(self, model_config, num_workers, cache_size, cache_mutex_buckets):
@@ -51,38 +36,24 @@ class HTMLTranslator:
         options.HTML = True
 
         # Get nodes. Replace them in place.
-        # document, errors = tidylib.tidy_document(page, BASE_OPTIONS)
         tree = html.fromstring(page)
 
-        head = tree.xpath("/html/head")[0]
-        for node in head.iterdescendants():
-            if node.text is None:
-                node.text = ''
-
-        body = tree.xpath("/html/body")[0]
-        for node in body.iterdescendants():
+        for node in tree.iterdescendants():
             if node.text is None:
                 node.text = ''
 
         imgs = tree.xpath("/html//img")
         print("Images to begin with", len(imgs))
 
-        node = html.fromstring(convert(body))
-        imgs = node.xpath("//img")
-        print("Images passed through tidy", len(imgs))
-
         if bypass:
-            response = FakeResponse(convert(body))
-            return self.postprocess(etree.tostring(head).decode("utf-8"), [None, response])
+            response = FakeResponse(convert(tree))
+            return self.postprocess(response)
         else:
-            responses = self.service.translate(self.model, bergamot.VectorString([convert(head), convert(body)]), options)
-            return self.postprocess(etree.tostring(head).decode("utf-8"), responses)
+            [response] = self.service.translate(self.model, bergamot.VectorString([convert(tree)]), options)
+            return self.postprocess(response)
         
-    def postprocess(self, original_head, responses):
-        head, body = responses
-        node = html.fromstring(body.target.text)
+    def postprocess(self, response):
+        node = html.fromstring(response.target.text)
         imgs = node.xpath("//img")
         print("Images at translated", len(imgs))
-        embed = '<html> {}  {} </html>'.format(original_head, body.target.text)
-        document, errors = tidylib.tidy_document(embed, BASE_OPTIONS)
-        return document
+        return response.target.text
--- tagtransfer/webapp.py
+++ tagtransfer/webapp.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask import request
 from lxml import html, etree
 import requests
-from .html_translator import HTMLTranslator, BASE_OPTIONS
+from .html_translator import HTMLTranslator
 import argparse
-import tidylib
+# from html import unescape
+# import re
 
 app = Flask(__name__)
 translator = None
@@ -13,13 +14,24 @@ translator = None
 def hello_world():
     url = request.args.get("url", "https://en.wikipedia.org/wiki/Physics")
     bypass = request.args.get("bypass", "false") == "true"
-    response = requests.get(url)
-    document = translator.translate(response.text, bypass)
+    response = requests.get(url, headers={
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15'
+    })
+
+    document = response.text
+    # Little hack for now: decode &#160; etc entities because we don't support those
+    # document = re.sub(r"&#\d+;", lambda match: unescape(match[0]), document)
+    
+    # Add <base href=""> to <head>
     tree = html.fromstring(document)
     head = tree.xpath('/html/head')[0]
     head.insert(1, html.fragment_fromstring('<base href="{}" target="_blank">'.format(url)))
-    document, errors = tidylib.tidy_document(etree.tostring(tree).decode("utf-8"), BASE_OPTIONS)
-    return document
+    
+    # Translate document HTML
+    translated = translator.translate(etree.tostring(tree, method='html', pretty_print=True, encoding='utf-8').decode(), bypass)
+
+    return f"<!DOCTYPE html>\n{translated}"
 
 
 if __name__ == "__main__":
