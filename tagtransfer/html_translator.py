ee37e9d09ea68e28d267e446c9820f17b79dc24f
Jerin Philip <jerinphilip@live.in>
2022-01-05 21:28:02 +0000

    Strip cleaning pipeline to bare minimum (#6)
    * Removing Fake(...) things by letting text ahead
    * Removing debug statements
    * Earlier workaround for empty tags no longer required
    * Remove another hula-hoop

--- tagtransfer/html_translator.py
+++ tagtransfer/html_translator.py
@@ -4,23 +4,12 @@ import cssselect
 from lxml import html, etree
 
 
-class FakeAnnotatedText:
-    def __init__(self, text):
-        self.text = text
-
-
-class FakeResponse:
-    def __init__(self, text):
-        self.target = FakeAnnotatedText(text)
-
-
 def convert(node):
     return etree.tostring(node, method="html", encoding="utf-8").decode("utf-8")
 
 
 class HTMLTranslator:
     def __init__(self, model_config, num_workers, cache_size, cache_mutex_buckets):
-        # TODO(jerinphilip): Improve syntax upstream.
         config = ServiceConfig(
             numWorkers=num_workers,
             cacheSize=cache_size,
@@ -33,30 +22,18 @@ class HTMLTranslator:
         self.model = self.service.modelFromConfigPath(model_config)
 
     def translate(self, page: str, bypass: bool = False):
-        # Hardcode a bunch of options for now. TODO: improve
         options = ResponseOptions(HTML=True)
 
         # Get nodes. Replace them in place.
         tree = html.fromstring(page)
 
-        for node in tree.iterdescendants():
-            if node.text is None:
-                node.text = ""
-
-        imgs = tree.xpath("/html//img")
-        print("Images to begin with", len(imgs))
-
         if bypass:
-            response = FakeResponse(convert(tree))
-            return self.postprocess(response)
+            return self.postprocess(convert(tree))
         else:
             [response] = self.service.translate(
                 self.model, bergamot.VectorString([convert(tree)]), options
             )
-            return self.postprocess(response)
+            return self.postprocess(response.target.text)
 
-    def postprocess(self, response):
-        node = html.fromstring(response.target.text)
-        imgs = node.xpath("//img")
-        print("Images at translated", len(imgs))
-        return response.target.text
+    def postprocess(self, text):
+        return text
