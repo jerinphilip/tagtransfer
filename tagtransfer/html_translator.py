e3edb738baf4373457b22f9b7167e6742903fbe8
Jerin Philip <jerinphilip@live.in>
2022-01-05 22:12:45 +0000

    Add a translate_url method to make HTMLTranslator more capable

--- tagtransfer/html_translator.py
+++ tagtransfer/html_translator.py
@@ -4,6 +4,7 @@ import cssselect
 from lxml import html, etree
 from bergamot.config import repository
 import typing as t
+import requests
 
 
 def convert(node):
@@ -61,3 +62,41 @@ class HTMLTranslator:
 
     def postprocess(self, text):
         return text
+
+    def intercept(self, url):
+        response = requests.get(
+            url,
+            headers={
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15",
+            },
+        )
+
+        document = response.text
+        # Little hack for now: decode &#160; etc entities because we don't support those
+        # document = re.sub(r"&#\d+;", lambda match: unescape(match[0]), document)
+
+        # Add <base href=""> to <head>
+        tree = html.fromstring(document)
+        head = tree.xpath("/html/head")[0]
+        head.insert(
+            1, html.fragment_fromstring('<base href="{}" target="_blank">'.format(url))
+        )
+
+        return etree.tostring(
+            tree, method="html", pretty_print=True, encoding="utf-8"
+        ).decode()
+
+    def translate_url(self, model1: str, model2: str, url: str, bypass: bool = False):
+        # Intercept the URL to obtain source and show translation
+        document = self.intercept(url)
+
+        # Translate document HTML
+        translated = self.translate(
+            model1,
+            model2,
+            document,
+            bypass,
+        )
+
+        return f"<!DOCTYPE html>\n{translated}"
