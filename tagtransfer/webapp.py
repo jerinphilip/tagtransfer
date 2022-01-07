3c39db833bee4a28e36674e9afe895765dc8a843
Jerin Philip <jerinphilip@live.in>
2022-01-07 11:00:24 +0000

    Inject ourselves into clickable links

--- tagtransfer/webapp.py
+++ tagtransfer/webapp.py
@@ -4,6 +4,7 @@ from lxml import html, etree
 import requests
 from .html_translator import HTMLTranslator
 import argparse
+import urllib
 
 # from html import unescape
 # import re
@@ -18,7 +19,32 @@ def index():
     bypass = request.args.get("bypass", "false") == "true"
     model1 = request.args.get("model", "en-de-tiny")
     model2 = request.args.get("pivot", None)
-    return translator.translate_url(model1, model2, url, bypass)
+    translated = translator.translate_url(model1, model2, url, bypass)
+
+    base_url = request.base_url
+
+    # I only need minimum clickable links transferred
+    def transform_url(u):
+        params = {
+            "url": u,
+            "model": model1,
+            "bypass": bypass,
+        }
+
+        if model2 is not None:
+            params["pivot"] = model2
+
+        paramstring = urllib.parse.urlencode(params)
+        return f"{base_url}/?{paramstring}"
+
+    tree = html.fromstring(translated)
+    anchors = tree.xpath("//a")
+    for anchor in anchors:
+        href = anchor.attrib.get("href", None)
+        if href:
+            anchor.attrib["href"] = transform_url(href)
+
+    return etree.tostring(tree, method="html", encoding="utf-8").decode("utf-8")
 
 
 if __name__ == "__main__":
@@ -38,4 +64,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     translator = HTMLTranslator(args.num_workers, args.cache_size)
-    app.run("0.0.0.0", "8080")
+    app.run("0.0.0.0", "8080", debug=True)
