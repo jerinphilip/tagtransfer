2c5889fc99637d4f8228e388387b02bfa042936c
Jerin Philip <jphilip@ed.ac.uk>
2022-01-09 13:47:49 +0000

    Get bypass correct

--- tagtransfer/webapp.py
+++ tagtransfer/webapp.py
@@ -29,7 +29,6 @@ def index():
             "url": u,
             "model": model1,
             "bypass": str(bypass).lower(),
-            "bypass": bypass,
         }
 
         if model2 is not None:
